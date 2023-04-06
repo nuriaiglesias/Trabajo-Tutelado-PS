9166cb74a52da6911f4177e8f4099daca244a407
alejandrabermo <alejandra.bermo@udc.es>
2023-04-06 18:21:58 +0200

    login screen (without functionality)

---
+++ app/src/main/java/es/udc/cookbook/LoginScreen.java
@@ -0,0 +1,35 @@
+package es.udc.cookbook;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginScreen extends AppCompatActivity{
+
+    EditText username, password;
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("user") && password.getText().toString().equals("hola")){
+                    Toast.makeText(LoginScreen.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginScreen.this, "arolas", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
